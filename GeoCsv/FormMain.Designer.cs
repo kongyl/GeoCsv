e26d6089f833b0a383d203996a9a22a6ecfb670c
kongyl <kongyl@126.com>
2020-01-15 15:47:37 +0800

    v1.0

---
+++ GeoCsv/FormMain.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace GeoCsv
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.buttonFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelState = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBarPercent = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabelPercent = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "数据路径：";
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Location = new System.Drawing.Point(83, 14);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.ReadOnly = true;
+            this.textBoxFolder.Size = new System.Drawing.Size(624, 21);
+            this.textBoxFolder.TabIndex = 1;
+            // 
+            // buttonFolder
+            // 
+            this.buttonFolder.Location = new System.Drawing.Point(713, 12);
+            this.buttonFolder.Name = "buttonFolder";
+            this.buttonFolder.Size = new System.Drawing.Size(75, 23);
+            this.buttonFolder.TabIndex = 2;
+            this.buttonFolder.Text = "浏览";
+            this.buttonFolder.UseVisualStyleBackColor = true;
+            this.buttonFolder.Click += new System.EventHandler(this.buttonFolder_Click);
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Location = new System.Drawing.Point(632, 41);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 3;
+            this.buttonOk.Text = "开始";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(713, 41);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "取消";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelState,
+            this.toolStripProgressBarPercent,
+            this.toolStripStatusLabelPercent});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 76);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelState
+            // 
+            this.toolStripStatusLabelState.Name = "toolStripStatusLabelState";
+            this.toolStripStatusLabelState.Size = new System.Drawing.Size(32, 17);
+            this.toolStripStatusLabelState.Text = "就绪";
+            // 
+            // toolStripProgressBarPercent
+            // 
+            this.toolStripProgressBarPercent.Name = "toolStripProgressBarPercent";
+            this.toolStripProgressBarPercent.Size = new System.Drawing.Size(100, 16);
+            this.toolStripProgressBarPercent.Visible = false;
+            // 
+            // toolStripStatusLabelPercent
+            // 
+            this.toolStripStatusLabelPercent.Name = "toolStripStatusLabelPercent";
+            this.toolStripStatusLabelPercent.Size = new System.Drawing.Size(26, 17);
+            this.toolStripStatusLabelPercent.Text = "0%";
+            this.toolStripStatusLabelPercent.Visible = false;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 98);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.buttonFolder);
+            this.Controls.Add(this.textBoxFolder);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GeoCsv";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.Button buttonFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelState;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBarPercent;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelPercent;
+    }
+}
+
---
+++ GeoCsv/FormMain.cs
@@ -0,0 +1,202 @@
+﻿using OSGeo.GDAL;
+using OSGeo.OSR;
+using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GeoCsv
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+
+            Gdal.AllRegister();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void buttonFolder_Click(object sender, EventArgs e)
+        {
+            textBoxFolder.Text = "";
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            string folder = textBoxFolder.Text;
+            if (folder == null || folder.Trim().Equals(""))
+            {
+                MessageBox.Show("请选择数据路径");
+                return;
+            }
+
+            toolStripStatusLabelState.Text = "正在处理";
+            toolStripProgressBarPercent.Visible = true;
+            toolStripStatusLabelPercent.Visible = true;
+            statusStrip1.Update();
+
+            // 获取元数据
+            string[] tifs = Directory.GetFiles(folder, "*.tif");
+            int tifNum = tifs.Length;
+            string[] fields = new string[tifNum];
+            Dataset[] datasets = new Dataset[tifNum];
+            DataType[] dataTypes = new DataType[tifNum];
+            double[] nodatas = new double[tifNum];
+            for (int i = 0; i < tifNum; i++)
+            {
+                // field
+                string[] paths = tifs[i].Split('\\');
+                string filename = paths[paths.Length - 1];
+                fields[i] = filename.Split('_')[0];
+                // dataset
+                datasets[i] = Gdal.Open(tifs[i], Access.GA_ReadOnly);
+                dataTypes[i] = datasets[i].GetRasterBand(1).DataType;
+                // nodata
+                int hasVal;
+                datasets[i].GetRasterBand(1).GetNoDataValue(out nodatas[i], out hasVal);
+                if (hasVal != 1)
+                {
+                    nodatas[i] = double.NaN;
+                }
+            }
+            double[] geoTransform = new double[6];
+            datasets[0].GetGeoTransform(geoTransform);
+            string proj = datasets[0].GetProjection();
+            SpatialReference sr = new SpatialReference(proj);
+            CoordinateTransformation trans = new CoordinateTransformation(sr, sr.CloneGeogCS());
+            int isGeographic = sr.IsGeographic();
+            double topLeftX = geoTransform[0];
+            double xRes = geoTransform[1];
+            double topLeftY = geoTransform[3];
+            double yRes = geoTransform[5];
+            int xSize = datasets[0].RasterXSize;
+            int ySize = datasets[0].RasterYSize;
+            int leftYSize = ySize;
+            int yOffset = 0;
+            int blockSize = 100;
+            int progressValue = 0;
+
+            // 写入csv
+            using (StreamWriter sw = new StreamWriter(folder + "\\output.csv"))
+            {
+                sw.WriteLine(getHeader(fields));
+                sw.Flush();
+                while (leftYSize > 0) // 按行分块
+                {
+                    int readYSize = (leftYSize > blockSize) ? blockSize : leftYSize;
+                    leftYSize = leftYSize - readYSize;
+                    double[][] data = new double[tifNum][];
+
+                    // 读入数据
+                    for (int i = 0; i < tifNum; i++)
+                    {
+                        data[i] = new double[xSize * readYSize];
+                        datasets[i].GetRasterBand(1).ReadRaster(0, yOffset, xSize, readYSize, data[i], xSize, readYSize, 0, 0);
+                    }
+                    for (int row = 0; row < readYSize; row++) // 按行
+                    {
+                        int rowNo = yOffset + row;
+                        int current = rowNo * 100 / ySize;
+                        if (current > progressValue) // 改变进度条状态
+                        {
+                            toolStripProgressBarPercent.Value = current;
+                            toolStripStatusLabelPercent.Text = string.Format("{0}%", current);
+                            statusStrip1.Update();
+                            progressValue = current;
+                        }
+                        for (int col = 0; col < xSize; col++) // 按列
+                        {
+                            StringBuilder builder = new StringBuilder();
+                            double[] latlon = new double[2];
+                            latlon[0] = topLeftX + col * xRes;
+                            latlon[1] = topLeftY + row * yRes;
+                            if (isGeographic == 0) // 投影转经纬度
+                            {
+                                trans.TransformPoint(latlon);
+                            }
+                            builder.Append(latlon[0].ToString("f6")).Append(",").Append(latlon[1].ToString("f6"));
+                            bool isNoData = false;
+                            for (int i = 0; i < tifNum; i++) // 按文件
+                            {
+                                builder.Append(",");
+                                double v = data[i][row * xSize + col];
+                                if (v == nodatas[i])
+                                {
+                                    isNoData = true;
+                                    break;
+                                }
+                                if (dataTypes[i] == DataType.GDT_Float32 || dataTypes[i] == DataType.GDT_Float64)
+                                {
+                                    builder.Append(v.ToString("f6"));
+                                }
+                                else
+                                {
+                                    builder.Append(v.ToString("f0"));
+                                }
+                            }
+                            if (!isNoData) // 非空值
+                            {
+                                sw.WriteLine(builder.ToString());
+                            }
+                        }
+                        sw.Flush();
+                    }
+
+                    yOffset = yOffset + readYSize;
+                }
+                sw.Close();
+            }
+
+            // 释放资源
+            for (int i = 0; i < tifNum; i++)
+            {
+                datasets[i].Dispose();
+            }            
+            toolStripStatusLabelState.Text = "就绪";
+            toolStripProgressBarPercent.Visible = false;
+            toolStripStatusLabelPercent.Visible = false;
+            statusStrip1.Update();
+            MessageBox.Show("处理完成");
+        }
+
+        private string getHeader(string[] fields)
+        {
+            StringBuilder builder = new StringBuilder("x,y");
+            for (int i = 0; i < fields.Length; i++)
+            {
+                builder.Append(",").Append(fields[i]);
+            }
+            return builder.ToString();
+        }
+
+        private string getFieldType(DataType type)
+        {
+            switch (type)
+            {
+                case DataType.GDT_Byte:
+                    return "byte";
+                case DataType.GDT_UInt16:
+                case DataType.GDT_Int16:
+                    return "short";
+                case DataType.GDT_UInt32:
+                case DataType.GDT_Int32:
+                    return "int";
+                case DataType.GDT_Float32:
+                    return "float";
+                case DataType.GDT_Float64:
+                    return "double";
+                default:
+                    return "other";
+            }
+        }
+    }
+}
